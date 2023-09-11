0c7d675d509b8fc27d286626c2596192bc259738
Carlos Pereira Atencio <carlos@microbit.org>
2023-09-11 19:41:58 +0100

    Fix missing file and expand macro test.

--- macrotest.cpp
+++ macrotest.cpp
@@ -5,16 +5,14 @@ using namespace pxt;
 #define MACROTOSTR(x) #x
 #define MACROTOSTRING(x) MACROTOSTR(x)
 
-#define MACROTOSTR20(x,y) #x
-#define MACROTOSTR21(x,y) #y
-#define MACROTOSTRING20(x) MACROTOSTR20(x)
-#define MACROTOSTRING21(x) MACROTOSTR21(x)
-
 namespace macrotest {
     //%
     void print_macros() {
-        uBit.serial.send("MICROBIT_BLE_UTILITY_SERVICE_PAIRING = " MACROTOSTRING(MICROBIT_BLE_UTILITY_SERVICE_PAIRING) "\n");
-        uBit.serial.send("MICROBIT_BLE_UTILITY_SERVICE         = " MACROTOSTRING(MICROBIT_BLE_UTILITY_SERVICE) "\n");
+        uBit.serial.send("MICROBIT_BLE_UTILITY_SERVICE_PAIRING     = " MACROTOSTRING(MICROBIT_BLE_UTILITY_SERVICE_PAIRING) "\n");
+        uBit.serial.send("MICROBIT_BLE_UTILITY_SERVICE             = " MACROTOSTRING(MICROBIT_BLE_UTILITY_SERVICE) "\n");
+        uBit.serial.send("RANDOM_MACRO_IN_YOTTA_CONFIG_CODAL       = " MACROTOSTRING(RANDOM_MACRO_IN_YOTTA_CONFIG_CODAL) "\n");
+        uBit.serial.send("RANDOM_MACRO_IN_YOTTA_CONFIG_MICROBITDAL = " MACROTOSTRING(RANDOM_MACRO_IN_YOTTA_CONFIG_MICROBITDAL) "\n");
+        uBit.serial.send("RANDOM_MACRO_IN_YOTTA_CONFIG             = " MACROTOSTRING(RANDOM_MACRO_IN_YOTTA_CONFIG) "\n");
         uBit.serial.send("\n");
     }
 }
